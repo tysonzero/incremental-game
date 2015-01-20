c74d2d6d4b306148bbbb86ee7b4d79e84ecc40fe
Tysonzero <danielspencersmith@gmail.com>
2015-01-20 14:28:12 -0700

    Create game.js

---
+++ scripts/game.js
@@ -0,0 +1,44 @@
+var canvas = document.getElementById('game_canvas');
+var context = canvas.getContext('2d');
+var keys = [];
+var lastKeys = [];
+var buttons = [];
+var lastButtons = [];
+var mousePos = {};
+
+canvas.width = 800;
+canvas.height = 600;
+
+window.addEventListener('keydown', function (event) {
+    keys[event.keyCode] = true;
+});
+
+window.addEventListener('keyup', function (event) {
+    keys[event.keyCode] = undefined;
+});
+
+canvas.addEventListener('mousedown', function (event) {
+    buttons[event.button] = true;
+    console.log(mousePos);
+});
+
+canvas.addEventListener('mouseup', function (event) {
+    buttons[event.button] = undefined;
+});
+
+canvas.addEventListener('mousemove', function (event) {
+    mousePos.x = event.offsetX;
+    mousePos.y = event.offsetY;
+});
+
+setInterval(
+    function () {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = '#FFFFFF';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        lastKeys = keys.slice();
+        lastButtons = buttons.slice();
+    },
+    10
+);
