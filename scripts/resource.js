54a7156061eb45602189a9651ebfb91f1e392e13
Tysonzero <danielspencersmith@gmail.com>
2015-01-21 23:42:57 -0700

    Create draw method on Resource prototype

--- scripts/resource.js
+++ scripts/resource.js
@@ -3,3 +3,11 @@ var Resource = function (name, quantity) {
     this.quantity = quantity;
 };
 
+Resource.prototype.draw = function (i) {
+    context.font = '30px Arial';
+    context.fillStyle = '#000000';
+    context.textBaseline = 'middle';
+    context.textAlign = 'right';
+    context.fillText(this.name + ': ' + this.quantity, 750, 50 + 50 * i);
+};
+
