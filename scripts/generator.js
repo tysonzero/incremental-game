e7d177faa90177e0659a9a476d069f8f1aa1924b
Tysonzero <danielspencersmith@gmail.com>
2015-01-24 22:33:55 -0700

    Create Generator.objects array

--- scripts/generator.js
+++ scripts/generator.js
@@ -5,3 +5,9 @@ var Generator = function (name, quantity, cost, output) {
     this.output = output;
 };
 
+Generator.objects = [
+    new Generator('Wooden Axe', 1, [100, 0], [5, 0]),
+    new Generator('Wooden Pickaxe', 0, [200, 0], [0, 1]),
+    new Generator('Stone Axe', 0, [0, 100], [10, 0]),
+    new Generator('Stone Pickaxe', 0, [0, 200], [0, 2])
+];
