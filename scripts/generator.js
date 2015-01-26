6f2c10ca5821fb57dc7a56c1c4520b687cd29885
Tysonzero <danielspencersmith@gmail.com>
2015-01-25 23:57:33 -0700

    Create isAffordable property on Generator

--- scripts/generator.js
+++ scripts/generator.js
@@ -5,6 +5,16 @@ var Generator = function (name, quantity, cost, output) {
     this.output = output;
 };
 
+Object.defineProperty(Generator.prototype, 'isAffordable', { get: function () {
+    var i = 0;
+    for (i = 0; i < this.cost.length; i++) {
+        if (this.cost[i] > Resource.objects[i].quantity) {
+            return false;
+        }
+    }
+    return true;
+}});
+
 Generator.objects = [
     new Generator('Wooden Axe', 1, [100, 0], [5, 0]),
     new Generator('Wooden Pickaxe', 0, [200, 0], [0, 1]),
