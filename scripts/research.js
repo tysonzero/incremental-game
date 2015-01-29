f32c83d2c7205e435d88e25b8c53dd462c82b966
Tysonzero <danielspencersmith@gmail.com>
2015-01-29 13:13:06 -0700

    Create draw method on Research prototype

--- scripts/research.js
+++ scripts/research.js
@@ -25,6 +25,28 @@ Research.prototype.purchase = function () {
     }
 };
 
+Research.prototype.draw = function (i) {
+    var count,
+        j;
+    if (mousePos.x >= 320 && mousePos.x < 620 && mousePos.y >= 10 + 75 * i && mousePos.y < 75 + 75 * i && this.isAffordable) {
+        context.fillStyle = '#333333';
+    } else {
+        context.fillStyle = '#000000';
+    }
+    context.fillRect(320, 10 + 75 * i, 300, 65);
+    context.fillStyle = this.isAffordable ? '#FFFFFF' : '#FF0000';
+    context.textBaseline = 'top';
+    context.textAlign = 'left';
+    context.font = '15px Arial';
+    context.fillText(this.name, 330, 14 + 75 * i);
+    for (j = 0, count = 0; j < this.cost.length; j++) {
+        if (this.cost[j]) {
+            context.fillText(Resource.objects[j].name + ': ' + this.cost[j], 330 + 100 * count, 34 + 75 * i);
+            count++;
+        }
+    }
+};
+
 Research.objects = [
     new Research('Wooden Tools', false, [100, 0], [0, 1]),
     new Research('Stone Tools', false, [0, 100], [2, 3])
