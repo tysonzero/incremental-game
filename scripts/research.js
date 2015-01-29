0ba9c2c9287d9ae971f8ea0a95708e88dc4b00f0
Tysonzero <danielspencersmith@gmail.com>
2015-01-29 13:07:40 -0700

    Create Research.objects array

--- scripts/research.js
+++ scripts/research.js
@@ -4,3 +4,8 @@ var Research = function (name, isPurchased, cost, unlocks) {
     this.cost = cost;
     this.unlocks = unlocks;
 };
+
+Research.objects = [
+    new Research('Wooden Tools', false, [100, 0], [0, 1]),
+    new Research('Stone Tools', false, [0, 100], [2, 3])
+];
