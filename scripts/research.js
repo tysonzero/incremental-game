76b51926a43b506650052bd8f612c9551ce527ee
Tysonzero <danielspencersmith@gmail.com>
2015-01-29 13:09:27 -0700

    Create isAffordable property on Research prototype

--- scripts/research.js
+++ scripts/research.js
@@ -5,6 +5,16 @@ var Research = function (name, isPurchased, cost, unlocks) {
     this.unlocks = unlocks;
 };
 
+Object.defineProperty(Research.prototype, 'isAffordable', { get: function () {
+    var i;
+    for (i = 0; i < this.cost.length; i++) {
+        if (this.cost[i] > Resource.objects[i].quantity) {
+            return false;
+        }
+    }
+    return true;
+}});
+
 Research.objects = [
     new Research('Wooden Tools', false, [100, 0], [0, 1]),
     new Research('Stone Tools', false, [0, 100], [2, 3])
