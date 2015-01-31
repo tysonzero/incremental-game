0a315891630b65a2e2bd162d75dac2c004f2f29b
Tysonzero <danielspencersmith@gmail.com>
2015-01-31 01:59:37 -0700

    Rename i to position in research.js

--- scripts/research.js
+++ scripts/research.js
@@ -25,29 +25,29 @@ Research.prototype.purchase = function () {
     }
 };
 
-Research.prototype.update = function (i) {
-    if (buttons[0] && !lastButtons[0] && mousePos.x >= 320 && mousePos.x < 620 && mousePos.y >= 10 + 75 * i && mousePos.y < 75 + 75 * i) {
+Research.prototype.update = function (position) {
+    if (buttons[0] && !lastButtons[0] && mousePos.x >= 320 && mousePos.x < 620 && mousePos.y >= 10 + 75 * position && mousePos.y < 75 + 75 * position) {
         this.purchase();
     }
 };
 
-Research.prototype.draw = function (i) {
+Research.prototype.draw = function (position) {
     var count,
         j;
-    if (mousePos.x >= 320 && mousePos.x < 620 && mousePos.y >= 10 + 75 * i && mousePos.y < 75 + 75 * i && this.isAffordable) {
+    if (mousePos.x >= 320 && mousePos.x < 620 && mousePos.y >= 10 + 75 * position && mousePos.y < 75 + 75 * position && this.isAffordable) {
         context.fillStyle = '#333333';
     } else {
         context.fillStyle = '#000000';
     }
-    context.fillRect(320, 10 + 75 * i, 300, 65);
+    context.fillRect(320, 10 + 75 * position, 300, 65);
     context.fillStyle = this.isAffordable ? '#FFFFFF' : '#FF0000';
     context.textBaseline = 'top';
     context.textAlign = 'left';
     context.font = '15px Arial';
-    context.fillText(this.name, 330, 14 + 75 * i);
+    context.fillText(this.name, 330, 14 + 75 * position);
     for (count = 0, j = 0; j < this.cost.length; j++) {
         if (this.cost[j]) {
-            context.fillText(Resource.objects[j].name + ': ' + this.cost[j], 330 + 100 * count, 34 + 75 * i);
+            context.fillText(Resource.objects[j].name + ': ' + this.cost[j], 330 + 100 * count, 34 + 75 * position);
             count++;
         }
     }
