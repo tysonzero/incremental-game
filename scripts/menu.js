538bec5c7c4cccc2be9041afac7b03b7f750b78e
Tysonzero <danielspencersmith@gmail.com>
2015-02-05 23:53:39 -0700

    Create Menu.objects array
    May need to rethink overall design. Classes are being treated like
    objects, which seems wrong despite the fact that they technically are
    objects.

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,3 +2,8 @@ var Menu = function (name, isActive) {
     this.name = name;
     this.isActive = isActive;
 };
+
+Menu.objects = [
+    new Menu('Generators', true),
+    new Menu('Research', false)
+];
