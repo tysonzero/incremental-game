33daabf32b9d86af81d2e71587553e800809a775
Tysonzero <danielspencersmith@gmail.com>
2015-02-09 23:58:17 -0700

    Add isAffordable property to PurchasableMixin

--- scripts/purchasable-mixin.js
+++ scripts/purchasable-mixin.js
@@ -1,2 +1,12 @@
 var PurchasableMixin = function () {};
 
+PurchasableMixin.prototype.isAffordable = { get: function () {
+    var i;
+    for (i = 0; i < this.costs.length; i++) {
+        if (this.costs[i] > this.resources[i].quantity) {
+            return false;
+        }
+    }
+    return true;
+}};
+
