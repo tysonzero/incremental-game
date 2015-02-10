4b1eed094b2b6df7b17192883230d61614d96b78
Tysonzero <danielspencersmith@gmail.com>
2015-02-10 12:35:48 -0700

    Add isUnlocked property to ResearchableMixin

--- scripts/researchable-mixin.js
+++ scripts/researchable-mixin.js
@@ -3,3 +3,12 @@ var ResearchableMixin = function (options) {
     this.requirements = options.requirements || [];
 };
 
+ResearchableMixin.prototype.isUnlocked = { get: function () {
+    var i;
+    for (i = 0; i < this.requirements.length; i++) {
+        if (!this.requirements[i].quantity) {
+            return false;
+        }
+    }
+    return true;
+}};
