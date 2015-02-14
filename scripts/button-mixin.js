2fe1cd3d050d642c209d8546c1d5ae33feb26a83
Tysonzero <danielspencersmith@gmail.com>
2015-02-13 23:23:26 -0700

    Add isFocused and isClicked to ButtonMixin

--- scripts/button-mixin.js
+++ scripts/button-mixin.js
@@ -3,3 +3,11 @@ var ButtonMixin = function (options) {
     this.pos = options.pos || {};
     this.size = options.size || {};
 };
+
+ButtonMixin.prototype.isFocused = { get: function () {
+    return mousePos.x >= this.pos.x && mousePos.x < this.pos.x + this.size.x && mousePos.y >= this.pos.y && mousePos.y < this.pos.y + this.size.y;
+}};
+
+ButtonMixin.prototype.isClicked = { get: function () {
+    return this.isFocused && buttons[0] && !lastButtons[0];
+}};
