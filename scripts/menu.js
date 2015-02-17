d277e4d31288f2f581a0130d6879948f0da9c6da
Tysonzero <danielspencersmith@gmail.com>
2015-02-16 23:51:46 -0700

    Copy Tab static methods on Menu prototype

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,3 +2,21 @@ var Menu = function (options) {
     options = options || {};
     this.tabs = options.tabs;
 };
+
+Menu.prototype.update = function () {
+    var i;
+    for (i = 0; i < this.tabs.length; i++) {
+        this.tabs[i].pos = {x: 10 + 110 * i, y: 10};
+        this.tabs[i].update();
+    }
+};
+
+Menu.prototype.draw = function () {
+    var i;
+    for (i = 0; i < this.tabs.length; i++) {
+        this.tabs[i].pos = {x: 10 + 110 * i, y: 10};
+        this.tabs[i].draw();
+    }
+};
+
+menu = new Menu({tabs: Tab.objects});
