6a9635ae025c6237d117ad32da6d959a51c44caa
Tysonzero <danielspencersmith@gmail.com>
2015-02-18 23:49:36 -0700

    Copy static Resource methods onto List prototype

--- scripts/list.js
+++ scripts/list.js
@@ -2,3 +2,23 @@ var List = function (options) {
     options = options || {};
     this.items = options.items || [];
 };
+
+List.prototype.update = function () {
+    var i;
+    for (i = 0; i < this.items.length; i++) {
+        if (this.items[i].isVisible) {
+            this.items[i].update();
+        }
+    }
+};
+
+List.prototype.draw = function () {
+    var position,
+        i;
+    for (position = 0, i = 0; i < this.items.length; i++) {
+        if (this.items[i].isVisible) {
+            this.items[i].draw(position);
+            position++;
+        }
+    }
+};
