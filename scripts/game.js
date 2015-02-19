a5615408e34b819cd322677f30bd6d1f81ad5d8c
Tysonzero <danielspencersmith@gmail.com>
2015-02-19 00:18:36 -0700

    Stop using Class.objects arrays
    No longer necessary.

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,35 @@
 (function () {
-    Resource.objects = [
+    var resources = [
         new Resource({name: 'Wood', quantity: 100}),
         new Resource({name: 'Stone'}),
         new Resource({name: 'Copper'})
     ];
 
-    Research.objects = [
-        new Research({name: 'Wooden Tools', resources: [Resource.objects[0]], costs: [100]}),
-        new Research({name: 'Stone Tools', resources: [Resource.objects[1]], costs: [100]}),
-        new Research({name: 'Copper Tools', resources: [Resource.objects[2]], costs: [100]})
+    var research = [
+        new Research({name: 'Wooden Tools', resources: [resources[0]], costs: [100]}),
+        new Research({name: 'Stone Tools', resources: [resources[1]], costs: [100]}),
+        new Research({name: 'Copper Tools', resources: [resources[2]], costs: [100]})
     ];
-    Research.objects[1].requirements.push(Research.objects[0]);
-    Research.objects[2].requirements.push(Research.objects[1]);
+    research[1].requirements.push(research[0]);
+    research[2].requirements.push(research[1]);
 
-    Generator.objects = [
-        new Generator({name: 'Wooden Axe', quantity: 1, resources: [Resource.objects[0]], costs: [150], outputs: [5], requirements: [Research.objects[0]]}),
-        new Generator({name: 'Wooden Pickaxe', resources: [Resource.objects[0], Resource.objects[1]], costs: [250, 0], outputs: [0, 2], requirements: [Research.objects[0]]}),
-        new Generator({name: 'Stone Axe', resources: [Resource.objects[0], Resource.objects[1]], costs: [50, 100], outputs: [10, 0], requirements: [Research.objects[1]]}),
-        new Generator({name: 'Stone Pickaxe', resources: [Resource.objects[0], Resource.objects[1], Resource.objects[2]], costs: [50, 200, 0], outputs: [0, 4, 1], requirements: [Research.objects[1]]}),
-        new Generator({name: 'Copper Axe', resources: [Resource.objects[0], Resource.objects[2]], costs: [50, 100], outputs: [25, 0], requirements: [Research.objects[2]]}),
-        new Generator({name: 'Copper Pickaxe', resources: [Resource.objects[0], Resource.objects[1], Resource.objects[2]], costs: [50, 0, 200], outputs: [0, 10, 3], requirements: [Research.objects[2]]})
-    ];
-
-    Tab.objects = [
-        new Tab({name: 'Generators', buttons: Generator.objects}),
-        new Tab({name: 'Research', buttons: Research.objects, isActive: true})
+    var generators = [
+        new Generator({name: 'Wooden Axe', quantity: 1, resources: [resources[0]], costs: [150], outputs: [5], requirements: [research[0]]}),
+        new Generator({name: 'Wooden Pickaxe', resources: [resources[0], resources[1]], costs: [250, 0], outputs: [0, 2], requirements: [research[0]]}),
+        new Generator({name: 'Stone Axe', resources: [resources[0], resources[1]], costs: [50, 100], outputs: [10, 0], requirements: [research[1]]}),
+        new Generator({name: 'Stone Pickaxe', resources: [resources[0], resources[1], resources[2]], costs: [50, 200, 0], outputs: [0, 4, 1], requirements: [research[1]]}),
+        new Generator({name: 'Copper Axe', resources: [resources[0], resources[2]], costs: [50, 100], outputs: [25, 0], requirements: [research[2]]}),
+        new Generator({name: 'Copper Pickaxe', resources: [resources[0], resources[1], resources[2]], costs: [50, 0, 200], outputs: [0, 10, 3], requirements: [research[2]]})
     ];
 
-    list = new List({items: Resource.objects});
+    var tabs = [
+        new Tab({name: 'Generators', buttons: generators}),
+        new Tab({name: 'Research', buttons: research, isActive: true})
+    ];
 
-    menu = new Menu({tabs: Tab.objects});
+    var list = new List({items: resources});
+
+    var menu = new Menu({tabs: tabs});
 
     update = function () {
         menu.update();
