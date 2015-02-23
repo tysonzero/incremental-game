e45af047f4a6db2f819010593dbd374b111def69
Tysonzero <danielspencersmith@gmail.com>
2015-02-22 23:52:06 -0700

    Use Array.prototype.filter to deactivate tabs

--- scripts/protos/menu.js
+++ scripts/protos/menu.js
@@ -4,17 +4,13 @@ var Menu = function (options) {
 };
 
 Menu.prototype.updateTabs = function () {
-    var i,
-        j;
+    var i;
     for (i = 0; i < this.tabs.length; i++) {
         this.tabs[i].pos = {x: 10 + 110 * i, y: 10};
         this.tabs[i].update();
-        if (this.tabs[i].isClicked) {
-            for (j = 0; j < this.tabs.length; j++) {
-                this.tabs[j].isActive = false;
-            }
-            this.tabs[i].isActive = true;
-        }
+    }
+    if (this.tabs.filter(function (tab) { return tab.isActive; }).length > 1) {
+        this.tabs.filter(function (tab) { return tab.isActive && !tab.isClicked; })[0].isActive = false;
     }
 };
 
